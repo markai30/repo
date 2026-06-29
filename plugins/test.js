977447ebf5e0c0674e765994646da4afa2d0217f
markai30 <matran999@gmail.com>
2026-06-29 22:11:27 +0700

    Create test.js

---
+++ plugins/test.js
@@ -0,0 +1,12 @@
+function getManifest() {
+    return JSON.stringify({
+        "id": "sportshots_pure_es5",          
+        "name": "Crophim Pro",
+        "description": "Phim Online",
+        "version": "1.3",             
+        "baseUrl": "https://sportshots.pro", 
+        "iconUrl": "https://sportshots.pro/wp-content/uploads/2026/04/phimhayok-io-fav.jpg", 
+        "isEnabled": true,
+        "type": "MOVIE"
+    });
+}
