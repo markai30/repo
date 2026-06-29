7f975196aebd8fbf76fbe62cffb08704ed02467e
markai30 <matran999@gmail.com>
2026-06-29 21:44:23 +0700

    Update crophim.js

--- plugins/crophim.js
+++ plugins/crophim.js
@@ -7,7 +7,7 @@ function getManifest() {
         "id": "crophim",          
         "name": "crophim",
         "description": "Phim Online",
-        "version": "1.1",             
+        "version": "1.2",             
         "baseUrl": "https://sportshots.pro", 
         "iconUrl": "https://sportshots.pro/wp-content/uploads/2026/04/phimhayok-io-fav.jpg", 
         "isEnabled": true,
