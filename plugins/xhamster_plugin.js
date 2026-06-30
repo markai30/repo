26d13c8271a493c80de8f71149054c87ca1f25d1
markai30 <matran999@gmail.com>
2026-07-01 05:57:43 +0700

    Update xhamster_plugin.js

--- plugins/xhamster_plugin.js
+++ plugins/xhamster_plugin.js
@@ -7,7 +7,7 @@ function getManifest() {
         "id": "xhamster",          
         "name": "Xhamster",
         "description": "XXX Hay",
-        "version": "1,2",             
+        "version": "2.1",             
         "baseUrl": "https://greenxh.today",
         "iconUrl": "https://static.cdnsolutions.media/xh-desktop/images/favicon/favicon-v2-256x256.ico", 
         "isEnabled": true,
@@ -191,14 +191,15 @@ function parseMovieDetail(html) {
 function parseDetailResponse(html) {
     try {
         var streamUrl = "";
-        /*
+        
         var rmatch = html.match(/rel="preload"\shref="([\s\S]*?m3u8)"/i);
    	 if (rmatch && rmatch[1]) { streamUrl = rmatch[1]; }
         var decodedUrl = streamUrl ? decodeURIComponent(streamUrl) : "";
-      */
+        
+      /*
       var rmatch = html.match(/link\s+rel="canonical"\s+href="([^"]+)"/i);
     if (rmatch && rmatch[1]) { lurl = rmatch[1]; }
-    
+    */
 		var customJs = `
 function initCustomVideoFix() {
   alert('${decodedUrl}');
@@ -245,9 +246,10 @@ if (document.readyState === 'loading') {
 `;
 
 return JSON.stringify({
-    url: lurl.replace("https://xhamster.com","https://greenxh.today"),
+    url: decodedUrl,
     headers: {
         "Referer": "https://greenxh.today",
+        "Origin": "https://greenxh.today",
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
         "Custom-Js": customJs.trim()
     }
